360eb82272c15d5861e2791b27e7c21c59916c61
Matias Salerno <matias.d.salerno@gmail.com>
2024-01-31 10:40:54 +0000

    initial commit

--- src/test/java/com/globant/javacodecamp/players/StatementPrinterTest.java
+++ src/test/java/com/globant/javacodecamp/players/StatementPrinterTest.java
@@ -21,7 +21,7 @@ class StatementPrinterTest {
             new Performance("othello", 40)));
 
     StatementPrinter statementPrinter = new StatementPrinter();
-    var actualStatement = statementPrinter.print(invoice, plays);
+    var actualStatement = statementPrinter.print(List.of(invoice), plays);
 
     var expectedStatement = """
             Statement for BigCo
@@ -29,7 +29,7 @@ class StatementPrinterTest {
             Amount owed is $500.00
             You earned 10 credits""";
 
-    assertEquals(expectedStatement, actualStatement);
+    assertEquals(expectedStatement, actualStatement.get(0));
   }
   @Test
   void statementForThreePlays() {
@@ -44,7 +44,7 @@ class StatementPrinterTest {
             new Performance("othello", 40)));
 
     StatementPrinter statementPrinter = new StatementPrinter();
-    var actualStatement = statementPrinter.print(invoice, plays);
+    var actualStatement = statementPrinter.print(List.of(invoice), plays);
 
     var expectedStatement = """
             Statement for BigCo
@@ -54,6 +54,44 @@ class StatementPrinterTest {
             Amount owed is $1,730.00
             You earned 47 credits""";
 
-    assertEquals(expectedStatement, actualStatement);
+    assertEquals(expectedStatement, actualStatement.get(0));
+  }
+
+  @Test
+  void statementForTwoInvoices() {
+    Map<String, Play> plays = Map.of(
+            "hamlet",  new Play("Hamlet", "tragedy"),
+            "as-like", new Play("As You Like It", "comedy"),
+            "othello", new Play("Othello", "tragedy"));
+
+    Invoice invoice1 = new Invoice("BigCo", List.of(
+            new Performance("hamlet", 55),
+            new Performance("as-like", 35),
+            new Performance("othello", 40)));
+
+
+    Invoice invoice2 = new Invoice("BigCo", List.of(
+            new Performance("othello", 40)));
+
+
+    StatementPrinter statementPrinter = new StatementPrinter();
+    var actualStatement = statementPrinter.print(List.of(invoice1, invoice2), plays);
+
+    var expectedStatement1 = """
+            Statement for BigCo
+              Hamlet: $650.00 (55 seats)
+              As You Like It: $580.00 (35 seats)
+              Othello: $500.00 (40 seats)
+            Amount owed is $1,730.00
+            You earned 47 credits""";
+
+    var expectedStatement2 = """
+            Statement for BigCo
+              Othello: $500.00 (40 seats)
+            Amount owed is $500.00
+            You earned 10 credits""";
+
+    assertEquals(expectedStatement1, actualStatement.get(0));
+    assertEquals(expectedStatement2, actualStatement.get(1));
   }
 }
